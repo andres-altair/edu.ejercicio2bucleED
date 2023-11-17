fed4ebd96c50920ba2aa27c720e6df21a6386d8f
DESKTOP-JOOKDG6\profesor <csi23-amoliaz@colegioaltair.net>
2023-11-17 10:07:51 +0100

    no message

--- src/Controladores/ejercicio2bucle.java
+++ src/Controladores/ejercicio2bucle.java
@@ -3,12 +3,28 @@ import Servicios.pedirI;
 import Servicios.pedirC;
 
 
-public class ejercicio2bucle {
+public class ejercicio2bucle 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		pedirI pI = new pedirC();
 		int i = pI.pedir();
-		for
-	}
+		int control ;
+		System.out.println("es primo 1   2");
+		for(int a = 3; a < i; i++) 
+			{
+				control = 0;
+				for (int j=2; j<i; j++) {
+				if(i%j==0) 
+				{
+					control=1;
+					break;
+				}
+				 break;
 
+				}
+			 if (control==0) {System.out.println("es primo  " + i);}
+		  }
+	}
 }
--- src/Servicios/pedirC.java
+++ src/Servicios/pedirC.java
@@ -7,7 +7,7 @@ public class pedirC implements pedirI {
 	@Override
 	public int pedir() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("escribr un numero entero");
+		System.out.println("escribe un numero entero");
 		int num = scan.nextInt();
 		return num;
 	}
